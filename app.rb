012b01c022e780d69513c0a6dba87229e23a0568
WordsPerMinute <contact@coreyhodge.net>
2020-01-10 11:07:43 -0700

    finished. curious to update later, using  diffrent variations of .max to see what works.

--- app.rb
+++ app.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class Company
     attr_accessor :name, :size
 
@@ -9,8 +11,37 @@ end
 
 companies = [
     Company.new('Alpha', 30),
-    Company.new('Beta, 300),
-    Company.new('Delta, 3000)
+    Company.new('Beta', 300),
+    Company.new('Delta', 3000)
 ]
 
+
 ### Your code below this
+
+def name_and_size(companies)
+    companies.map do |company|
+        "#{company.name} - #{company.size}"
+    end
+end
+
+def combine_all_sizes(companies)
+    companies.reduce(0) {|sum, company| sum + company.size}
+end
+
+def companies_over_100(companies)
+    companies.select do |company|
+        company.size > 100
+    end
+end
+
+def find_beta_company(companies)
+    companies.find do |company|
+        company.name = "Beta"
+    end
+end
+
+def find_largest_company(companies)
+    companies.max(1) do |company|
+        company.size
+    end
+end
